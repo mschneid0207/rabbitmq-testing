234e784f773590a933f06e24affe86f447c54b5c
Markus Schneider <markus.schneider0207@gmail.com>
2018-10-28 13:58:47 +0100

    topic exchange

---
+++ src/main/java/de/mschneid/rabbitmq/tut5/Tut5Sender.java
@@ -0,0 +1,38 @@
+package de.mschneid.rabbitmq.tut5;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class Tut5Sender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private TopicExchange topic;
+
+    private int index;
+
+    private int count;
+
+    private final String[] keys = {"quick.orange.rabbit",
+            "lazy.orange.elephant", "quick.orange.fox",
+            "lazy.brown.fox", "lazy.pink.rabbit", "quick.brown.fox"};
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if (++this.index == keys.length) {
+            this.index = 0;
+        }
+        String key = keys[this.index];
+        builder.append(key).append(' ');
+        builder.append(Integer.toString(++this.count));
+        String message = builder.toString();
+        rabbitTemplate.convertAndSend(topic.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
